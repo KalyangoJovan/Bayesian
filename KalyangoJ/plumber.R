da661d0ffa74d92905d42ced6887e9ec084bdb1f
KalyangoJovan <kalyangojovann@gmail.com>
2022-05-08 10:06:09 +0200

    third commit

---
+++ KalyangoJ/plumber.R
@@ -0,0 +1,6 @@
+
+library(plumber)
+
+#* @apiTitle Bayesian Statistics Exam 
+#* @apiDescription This will take place at Utrecht University on 6th APril 2022
+#* @Using ssesa as the data set for the analysis
